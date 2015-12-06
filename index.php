5aa1fe094533a922bbdddde746d3b4f297950a8d
Yury Egoshin <me@egoshin.net>
2015-12-06 21:15:31 +0300

    Landing Page v.1.0 06.12.2015

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="DiTech Service">
+    <meta name="author" content="DiTech Service">
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <title>DiTech Service</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Переключение навигации</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav logo" href="#"></a>
+                <div class="nav navbar-nav header-info">
+                    <a class="header-mailto" href="mailto:mail@ditechservice.ru">mail@ditechservice.ru</a><br>
+                    <a class="header-phone" href="callto:+74954659353">(495) 465-93-53</a>
+                </div>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right main-menu">
+                    <li>
+                        <a href="#about">О Компании</a>
+                    </li>
+                    <li>
+                        <a href="#services">Услуги</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>DiTech Service</h1>
+                        <h3>Наша специализация – перевод документов в электронный вид.</h3>
+                        <hr class="intro-divider">
+                        <p class="lead">Мы предоставляем услуги по сканированию документов, созданию электронных архивов,<br>
+                            обработке фотоматериалов, распознаванию текста и т.д.</p>
+                        <p>Сотрудничаем с архивами, издательствами, редакциями периодических изданий.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+
+    <!-- Page Content -->
+
+	<a  name="services"></a>
+    <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>Услуги</h2>
+                </div>
+            </div>
+            <div class="alert alert-success">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="/img/ipad.png" alt="Сканирование документов">
+                    </div>
+                    <div class="media-body media-middle">
+                        <p class="leader">Сканирование документов (архивные документы, в том числе плохой
+                            сохранности, книги, фотографии, карты, широкоформатные материалы)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="alert alert-info">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="/img/dog.png" alt="Сканирование фотоматериалов">
+                    </div>
+                    <div class="media-body media-middle">
+                        <p class="leader">Сканирование фотоматериалов</p>
+                    </div>
+                </div>
+            </div>
+            <div class="alert alert-warning">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="/img/phones.png" alt="Перевод документов в электронный вид">
+                    </div>
+                    <div class="media-body media-middle">
+                        <p class="leader">Перевод документов в электронный вид (сканирование документа и
+                            распознавание текста)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="alert alert-danger">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="/img/ipad.png" alt="Набор текста">
+                    </div>
+                    <div class="media-body media-middle">
+                        <p class="leader">Набор текста</p>
+                    </div>
+                </div>
+            </div>
+            <div class="alert alert-success">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="/img/dog.png" alt="Переводы">
+                    </div>
+                    <div class="media-body media-middle">
+                        <p class="leader">Переводы</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- /.content-section-a -->
+	<a  name="contact"></a>
+    <div class="contact-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>Контакты</h2>
+                </div>
+                <div class="col-xs-12 contacts">
+                    <p><b>Адрес: </b>&nbsp;<span class="lead">105203 г. Москва, ул. Парковая 16-я, д. 5</span></p>
+                    <p><b>Телефон: </b>&nbsp;<a href="callto:+74954659353" class="lead">+7(495)465-93-53</a></p>
+                    <p><b>Написать нам: </b>&nbsp;<a href="mailto:mail@ditechservice.ru" class="lead">mail@ditechservice.ru</a></p>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <div class="map img-responsive">
+        <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=TwIBr_bnfgD5FtHysEd2gQ35ZIJopW2J&width=100%&height=400&lang=ru_RU&sourceType=constructor"></script>
+    </div>
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-4 footer-col-1">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#about">О компании</a>
+                        </li>
+                        <li>
+                            <a href="#services">Услуги</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Контакты</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 footer-col-2 text-center">
+                    <a class="header-mailto" href="mailto:mail@ditechservice.ru">mail@ditechservice.ru</a><br>
+                    <a class="header-phone" href="callto:+74954659353">(495) 465-93-53</a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 footer-col-3 text-right">
+                    <p class="copyright text-muted small">&copy;<?=date('Y')?> ООО «ДиТек Сервис»</p>
+                    <p class="text-muted small">г. Москва, ул. Парковая 16-я, д. 5</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
